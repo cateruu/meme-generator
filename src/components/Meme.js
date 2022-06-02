f82e07de54fc05ec760f0903a30a7ad8aa72acbb
Paweł Kromołowski <cater981pl@gmail.com>
2022-06-02 16:09:55 +0200

    Getting random meme img

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,28 @@
 import React from "react";
+import memeData from "../memeData";
 
 const Form = () => {
+
+  const getImg = () => {
+    const memes = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * 100);
+    const imgUrl = memes[randomNumber].url;
+
+    console.log(imgUrl);
+  }
+
   return (
     <main className="meme">
       <form className="meme__input-container">
         <input type="text" className="meme__input-container__input" placeholder="Top text" />
         <input type="text" className="meme__input-container__input" placeholder="Bottom text" />
       </form>
-        <button className="meme__button">Get a new meme image</button>
+        <button 
+        className="meme__button"
+        onClick={getImg}
+        >
+          Get a new meme image
+        </button>
 
     </main>
   );
